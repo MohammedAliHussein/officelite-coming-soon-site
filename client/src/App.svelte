4de45517dcd2435088766143ca9d0a1dc882ae11
Mohammed-Ali Hussein <mohammedalihussein02@gmail.com>
2022-07-09 10:07:27 +0800

    Theme added

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,9 +3,21 @@
 </script>
 
 <main>
-
+  <div class="a">
+    <h1>asd</h1>
+  </div>
+  <h1>asd</h1>
 </main>
 
-<style>
-
+<style lang="scss">
+  main {
+    .a {
+      h1 {
+        color: black;
+      }
+    }
+    h1 {
+      color: yellow;
+    }
+  }
 </style>
